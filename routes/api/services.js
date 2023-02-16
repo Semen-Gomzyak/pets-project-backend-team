6c082a316980378fb0819318c0932032d80236d2
Yuliia Fedorychak <Yulyafed86@gmail.com>
2023-02-16 14:31:40 +0200

    add services

--- routes/api/services.js
+++ routes/api/services.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const { tryCatchWrapper } = require('../../middlwares');
 
+const { getServices } = require('../../controllers/services');
 
-
+router.get("/", tryCatchWrapper(getServices));
 
 module.exports = router;
