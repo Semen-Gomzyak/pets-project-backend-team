9a912cd770c9f11e618a2d19ceb302a58ade52c0
Andriy Ivanenko <andriy.ivanenko@gmail.com>
2023-02-16 15:54:01 +0200

    Update index.js

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -1,12 +1,11 @@
-const updateUser = require('./updateUser');
-
-module.exports = { updateUser };
 const userRegistration = require('./userRegistration');
 const userLogin = require('./userLogin.js');
 const getUserById = require('./getUserById');
+const updateUser = require('./updateUser');
 
 module.exports = {
   userRegistration,
   userLogin,
   getUserById,
+  updateUser,
 };
