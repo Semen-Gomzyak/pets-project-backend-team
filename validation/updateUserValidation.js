607f0e2661bf252588c9cda5d9d7bab60de04ada
Andriy Ivanenko <andriy.ivanenko@gmail.com>
2023-02-17 00:26:55 +0200

    updateUser refactored

--- validation/updateUserValidation.js
+++ validation/updateUserValidation.js
@@ -1,14 +1,13 @@
 const Joi = require('joi');
 
 const updateUserValidate = Joi.object({
-  name: Joi.string().min(2).max(128).required(),
-  email: Joi.string().email().required(),
-  mobilePhone: Joi.string()
-    .regex(/^\+380\d{9}$/)
-    .required(),
-  cityRegion: Joi.string()
-    .regex(/^[A-Za-z]+,[\sA-Za-z]+$/)
-    .required(),
+  name: Joi.string().min(3).max(20),
+  email: Joi.string().email(),
+  mobilePhone: Joi.string().regex(/^\+380\d{9}$/),
+  cityRegion: Joi.string().regex(/^[A-Za-z]+,[\sA-Za-z]+$/),
+  birthday: Joi.string().regex(
+    /(3[01]|[12][0-9]|0[1-9]).(1[0-2]|0[1-9]).[0-9]{4}$/,
+  ),
 });
 
 module.exports = updateUserValidate;
