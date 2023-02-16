b189616c51173931b2ace1af7217b37655c979ea
Helena Gingembre <e.izotovagingembre@outlook.com>
2023-02-16 16:39:27 +0100

    bugfix news

--- controllers/news/index.js
+++ controllers/news/index.js
@@ -4,16 +4,15 @@ const { HttpError } = require('../../middlwares/index');
 const getNews = async (req, res) => {
   const { page = 1, limit = 6 } = req.query;
   const skip = (page - 1) * limit;
-
-  const result = await News.find({}, '', { skip, limit });
+  console.log('test1');
+  // const result = await News.find({}, '', { skip, limit });
+  const result = await News.find({});
 
   if (!result) {
     throw HttpError(404);
   }
-
+  console.log('test2');
   res.status(200).json(result);
 };
 
-module.exports = {
-  getNews,
-};
+module.exports = getNews;
