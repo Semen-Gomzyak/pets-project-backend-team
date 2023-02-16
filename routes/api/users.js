03296024c114c80cfc0ba69d919f857b60cd336b
Andriy Ivanenko <andriy.ivanenko@gmail.com>
2023-02-16 15:30:28 +0200

    profile endpoints

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const tryCatchWrapper = require('../../middlwares/tryCatchWrapper');
 
+const { validateRegistration, loginValidation } = require('../../validation');
+const validateBody = require('../../middlwares/ValidateBody');
+const auth = require('../../middlwares/auth');
 
+const {
+  userRegistration,
+  userLogin,
+  getUserById,
+} = require('../../controllers/users');
 
-
+router.post(
+  '/register',
+  validateBody(validateRegistration),
+  tryCatchWrapper(userRegistration),
+);
+router.get('/login', validateBody(loginValidation), tryCatchWrapper(userLogin));
+router.get('/:userId', auth, tryCatchWrapper(getUserById));
 
 module.exports = router;
