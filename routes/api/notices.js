6d7f521f78627b11032a9e93e3ad26995f65d443
Yuliia Fedorychak <Yulyafed86@gmail.com>
2023-02-16 18:25:17 +0200

    Update notices.js

--- routes/api/notices.js
+++ routes/api/notices.js
@@ -2,16 +2,16 @@ const express = require('express');
 const tryCatchWrapper = require('../../middlwares/tryCatchWrapper');
 const validateBody = require('../../middlwares/ValidateBody');
 const validateNotice = require('../../validation/noticeValidation');
-const { notices: enpoint } = require('../../controllers')
+// const { notices: enpoint } = require('../../controllers')
 
-const { addNotice, getNoticesByCategory, getByCategoryAndTitle } = enpoint;
+// const { addNotice, getNoticesByCategory, getByCategoryAndTitle } = enpoint;
 const router = express.Router();
 
 const validatePost = validateBody(validateNotice);
 
-router.post('/category/:category', validatePost, tryCatchWrapper(addNotice));
-router.get('/category/:category', tryCatchWrapper(getNoticesByCategory));
-router.get('/:category/:title', tryCatchWrapper(getByCategoryAndTitle));
+// router.post('/category/:category', validatePost, tryCatchWrapper(addNotice));
+// router.get('/category/:category', tryCatchWrapper(getNoticesByCategory));
+// router.get('/:category/:title', tryCatchWrapper(getByCategoryAndTitle));
 
 
 module.exports = router;
