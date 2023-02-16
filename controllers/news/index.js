81b0f835937f12989ee1964cfe820ed746038e2c
Helena Gingembre <e.izotovagingembre@outlook.com>
2023-02-16 15:11:35 +0100

    add news router, ctrl

---
+++ controllers/news/index.js
@@ -0,0 +1,19 @@
+const { News } = require('../../models/news');
+const { HttpError } = require('../../middlwares/index');
+
+const getNews = async (req, res) => {
+  const { page = 1, limit = 6 } = req.query;
+  const skip = (page - 1) * limit;
+
+  const result = await News.find({}, '', { skip, limit });
+
+  if (!result) {
+    throw HttpError(404);
+  }
+
+  res.status(200).json(result);
+};
+
+module.exports = {
+  getNews,
+};
--- routes/api/notices.js
+++ routes/api/notices.js
@@ -2,14 +2,13 @@ const express = require('express');
 const tryCatchWrapper = require('../../middlwares/tryCatchWrapper');
 const validateBody = require('../../middlwares/ValidateBody');
 const validateNotice = require('../../validation/noticeValidation');
-const { addNotice } = enpointFn;
+// const { addNotice } = enpointFn;
 const router = express.Router();
 
 const validatePost = validateBody(validateNotice);
 
-router.post('/category/:category', validatePost, tryCatchWrapper(addNotice));
-router.get('/category/:category',);
-router.get('/:category/:title',);
-
+// router.post('/category/:category', validatePost, tryCatchWrapper(addNotice));
+router.get('/category/:category');
+router.get('/:category/:title');
 
 module.exports = router;
