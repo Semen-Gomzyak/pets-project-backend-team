ff055b4a34ad773534f8419faa8c2e984966bdb8
Helena Gingembre <e.izotovagingembre@outlook.com>
2023-02-16 16:46:27 +0100

    add limit news

--- controllers/news/index.js
+++ controllers/news/index.js
@@ -5,8 +5,7 @@ const getNews = async (req, res) => {
   const { page = 1, limit = 6 } = req.query;
   const skip = (page - 1) * limit;
   console.log('test1');
-  // const result = await News.find({}, '', { skip, limit });
-  const result = await News.find({});
+  const result = await News.find({}, '', { skip, limit });
 
   if (!result) {
     throw HttpError(404);
