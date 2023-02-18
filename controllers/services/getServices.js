5bb667fd810d85fa5c4dc57ca561fd9a5daa27c9
Yuliia Fedorychak <Yulyafed86@gmail.com>
2023-02-19 00:23:50 +0200

    Update getServices.js

--- controllers/services/getServices.js
+++ controllers/services/getServices.js
@@ -11,8 +11,21 @@ const getServices = async (req, res) => {
         throw NewError(404, 'Services not found');
     }
 
-    const workDays = services.map(service => service.workDays);
-    res.status(200).json(workDays);
+    const mappedDays = services.map(service => {
+        if (service.workDays !== null) {
+            const mappedWorkDays = service.workDays.map(workDay => {
+                console.log(workDay)
+                return { ...workDay, from: workDay.from, to: workDay.to };
+            });
+            return { ...service, workDays: mappedWorkDays };
+        } else {
+            return service;
+        }
+        });
+    
+    
+
+    res.status(200).json( services ); 
 };
 
 module.exports = getServices;
