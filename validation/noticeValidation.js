52c3a820240b1c39614c62c4a691f62833eed31d
Semen-Gomzyak <Blacklife1817@gmail.com>
2023-02-26 05:57:30 +0100

    change format date

--- validation/noticeValidation.js
+++ validation/noticeValidation.js
@@ -4,10 +4,7 @@ const moment = require('moment');
 const validateNotice = Joi.object({
   title: Joi.string().min(2).max(48).required(),
   name: Joi.string().min(2).max(24).required(),
-  birthdate: Joi.date()
-.greater(moment().subtract(20, 'years').format('YYYY-MM-DD'))
-.less(moment().format('YYYY-MM-DD'))
-.required('valid format date : YYYY-MM-DD'),
+  birthdate: Joi.string(),
   breed: Joi.string().min(2).max(24),
   location: Joi.string().required(),
   theSex: Joi.string().required(),
