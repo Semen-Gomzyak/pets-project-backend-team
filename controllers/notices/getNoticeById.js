faa278d492469eff8a96a4a25c0ae37d1398d12b
Semen-Gomzyak <Blacklife1817@gmail.com>
2023-02-26 18:35:49 +0100

    change get notice by id

--- controllers/notices/getNoticeById.js
+++ controllers/notices/getNoticeById.js
@@ -1,10 +1,40 @@
 const { Notice } = require('../../models');
+const { User } = require('../../models');
 
 const getNoticeById = async (req, res, next) => {
   const { noticeId } = req.params;
   const currentNotice = await Notice.findById(noticeId);
-
-  return res.status(201).json(currentNotice);
+  const {owner} = currentNotice;
+    const user = await User.findById(owner);
+    const email = user.email;
+    const phone = user.mobilePhone;
+    const {
+      _id,
+      title,
+      name,
+      birthdate,
+      breed,
+      location,
+      theSex,
+      comments,
+      price,
+      avatarURL
+    } = currentNotice;
+  console.log(user);
+  return res.status(201).json({
+    email,
+    phone,
+    _id,
+    title,
+    name,
+    birthdate,
+    breed,
+    location,
+    theSex,
+    comments,
+    price,
+    avatarURL,
+  });
 };
 
 module.exports = getNoticeById;
