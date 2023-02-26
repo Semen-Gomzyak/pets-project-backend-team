05a66c57c6edab8f25ecf8f81f754dfcf95ccfb6
Semen-Gomzyak <Blacklife1817@gmail.com>
2023-02-26 06:01:13 +0100

    delete price

--- validation/noticeValidation.js
+++ validation/noticeValidation.js
@@ -9,7 +9,6 @@ const validateNotice = Joi.object({
   location: Joi.string().required(),
   theSex: Joi.string().required(),
   comments: Joi.string().min(8).max(120).required(),
-  price: Joi.number().positive(),
   category: Joi.string(),
 });
 
