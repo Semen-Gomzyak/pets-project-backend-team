aab1bb16e68781e175c50064a84870a74bf32f51
Semen-Gomzyak <Blacklife1817@gmail.com>
2023-02-26 16:59:33 +0100

    get email and phone

--- controllers/notices/getNoticeById.js
+++ controllers/notices/getNoticeById.js
@@ -1,9 +1,17 @@
 const { Notice } = require('../../models');
+const { User } = require('../../models');
 
 const getNoticeById = async (req, res, next) => {
   const { noticeId } = req.params;
   const currentNotice = await Notice.findById(noticeId);
-  return res.status(201).json(currentNotice);
+  const { owner } = currentNotice;
+  const findOwner = await User.find(owner);
+  const email = findOwner[0].email;
+  const phone = findOwner[0].mobilePhone; 
+
+  return res
+    .status(201)
+    .json({ notice: currentNotice, email, phone });
 };
 
 module.exports = getNoticeById;
