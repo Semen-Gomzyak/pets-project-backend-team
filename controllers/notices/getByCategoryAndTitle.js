e30fa923def7d6dd41291d73ac2c4c97e04f1af7
Semen-Gomzyak <Blacklife1817@gmail.com>
2023-02-27 22:20:19 +0100

    delete sengrid key

--- controllers/notices/getByCategoryAndTitle.js
+++ controllers/notices/getByCategoryAndTitle.js
@@ -2,6 +2,7 @@ const { Notice } = require('../../models');
 
 const getByCategoryAndTitle = async (req, res) => {
   console.log(req.params);
+  console.log(req.query);
   const { title, category } = req.params;
   const { page = 1, limit = 8 } = req.query;
   const skip = (page - 1) * limit;
@@ -17,11 +18,11 @@ const getByCategoryAndTitle = async (req, res) => {
   );
   
   if (filteredNotices.length === 0) {
-    res.status(400).json('Noitice not found');
+    res.status(400).json('Noitice not found'); 
   }
 
   res.status(200).json({
-    filteredNotices,
+    filteredNotices
   });  
     
 
