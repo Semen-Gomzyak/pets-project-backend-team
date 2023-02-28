4a7cc65c6f61eec1c75257f5670787b6264c4f64
Semen-Gomzyak <Blacklife1817@gmail.com>
2023-02-28 14:26:34 +0100

    change get Notice By ID and Category

--- controllers/notices/getNoticesByCategory.js
+++ controllers/notices/getNoticesByCategory.js
@@ -1,16 +1,28 @@
 const { Notice } = require('../../models');
 
-const getNoticesByCategory = async (req, res) => {
-  const { category } = req.params;
-  const { page = 1, limit = 10 } = req.query;
+const getByCategoryAndTitle = async (req, res) => {
+  const { title, category } = req.params;
+  const { page = 1, limit = 8 } = req.query;
   const skip = (page - 1) * limit;
   const notices = await Notice.find({ category }, '', {
     skip,
     limit: Number(limit),
+    sort: { createdAt: -1 },
   }).populate('category');
-  const total = await Notice.countDocuments({ category }); 
 
-  res.status(200).json({ total, notices });
+  const filteredNotices = notices.filter(
+    notice =>
+      notice.title.toLowerCase().includes(title.toLowerCase()) &&
+      notice.category.toLowerCase() === category.toLowerCase(),
+  );
+
+  if (filteredNotices.length === 0) {
+    res.status(400).json('Notice not found');
+  }
+
+  res.status(200).json({
+    filteredNotices: [...filteredNotices].reverse(),
+  });
 };
 
-module.exports = getNoticesByCategory;
+module.exports = getByCategoryAndTitle;
